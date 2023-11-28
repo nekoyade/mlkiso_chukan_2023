7adbdff7383860daaf396be373d8dd2f86cba3d4
nekoyade <ktr.km.sch@gmail.com>
2023-11-29 04:00:44 +0900

    Create CustomCNN class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -69,3 +70,92 @@ plt.show()
 train_features, train_labels = next(iter(training_data_loader))
 print(f"feature batch size: {train_features.size()}")
 print(f"label batch size: {train_labels.size()}")
+
+
+# モデルによるデータ変換の可視化
+
+sample_images = []
+
+rows, cols = 4, 8
+for i in range(rows):
+    tmp_list = []
+    for j in range(cols):
+        sample_index = torch.randint(len(training_data), size=(1,)).item()
+        image, _ = training_data[sample_index]
+        tmp_list.append(image)
+    sample_images.append(tmp_list)
+
+def show_concatenated_images(image_list_2d, title):
+    rows = len(image_list_2d)
+
+    image_rows = []
+    for i in range(rows):
+        image_rows.append(torch.cat(image_list_2d[i], dim=2))
+    concatenated_image = torch.cat(image_rows, dim=1)
+
+    channels = concatenated_image.size()[0]
+    concatenated_image = torch.sum(concatenated_image, dim=0) / channels
+
+    plt.figure(figsize=(10, 10))
+    plt.title(title)
+    plt.imshow(concatenated_image.detach().numpy(), cmap="viridis")
+
+show_concatenated_images(sample_images, "sample images")
+
+f = nn.Conv2d(3, 32, kernel_size=3, stride=1)
+for i in range(rows):
+    for j in range(cols):
+        sample_images[i][j] = f(sample_images[i][j])
+
+show_concatenated_images(
+    sample_images, "nn.Conv2d(3, 32, kernel_size=3, stride=1)")
+
+f = nn.MaxPool2d(kernel_size=2)
+for i in range(rows):
+    for j in range(cols):
+        sample_images[i][j] = f(sample_images[i][j])
+
+show_concatenated_images(
+    sample_images, "nn.MaxPool2d(kernel_size=2)")
+
+f = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+for i in range(rows):
+    for j in range(cols):
+        sample_images[i][j] = f(sample_images[i][j])
+
+show_concatenated_images(
+    sample_images, "nn.Conv2d(32, 64, kernel_size=3, stride=1)")
+
+f = nn.MaxPool2d(kernel_size=2)
+for i in range(rows):
+    for j in range(cols):
+        sample_images[i][j] = f(sample_images[i][j])
+
+show_concatenated_images(
+    sample_images, "nn.MaxPool2d(kernel_size=2)")
+
+
+# 訓練に使用するデバイスを確認
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"using {device} device for training")
+
+
+# モデルの定義
+
+class CustomCNN(nn.Module):
+    def __init__(self):
+        super(CustomCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = nn.ReLU(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = nn.ReLU(x)
+        # Add more... (referred to AlexNet)
